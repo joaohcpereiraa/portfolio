aedc3005cc3517ebf79706d7283a314205442479
João Pereira <joao.costa.20@hotmail.com>
2026-03-07 18:39:42 +0000

    Removed old mock data

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,28 +116,7 @@ const dockApps = [
 ];
 
 const blogPosts = [
-  {
-    id: 1,
-    date: "Sep 2, 2025",
-    title:
-      "TypeScript Explained: What It Is, Why It Matters, and How to Master It",
-    image: "/images/blog1.png",
-    link: "https://jsmastery.com/blog/typescript-explained-what-it-is-why-it-matters-and-how-to-master-it",
-  },
-  {
-    id: 2,
-    date: "Aug 28, 2025",
-    title: "The Ultimate Guide to Mastering Three.js for 3D Development",
-    image: "/images/blog2.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-three-js-for-3d-development",
-  },
-  {
-    id: 3,
-    date: "Aug 15, 2025",
-    title: "The Ultimate Guide to Mastering GSAP Animations",
-    image: "/images/blog3.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-gsap-animations",
-  },
+ 
 ];
 
 const techStack = [
@@ -417,7 +396,7 @@ const ABOUT_LOCATION = {
     },
     {
       id: 4,
-      name: "about-me.txt",
+      name: "about-me.tfxt",
       icon: "/images/txt.png",
       kind: "file",
       fileType: "txt",
